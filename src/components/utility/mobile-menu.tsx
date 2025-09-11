a47dec0a4b89f533f011427f52424ce5ab9998b4
Nirosh Shetty <niroshshetty2020@gmail.com>
2025-09-11 13:31:30 +0530

    minor change( year changes from 2023 to 2025 in mobile menu)

--- src/components/utility/mobile-menu.tsx
+++ src/components/utility/mobile-menu.tsx
@@ -58,7 +58,7 @@ export default function MobileMenu({
                 ))}
                 <ThemeSwitch setClose={setOpenMenu} />
               </div>
-              <div className="absolute bottom-0 py-6">©2023 Nirosh Shetty</div>
+              <div className="absolute bottom-0 py-6">©2025 Nirosh Shetty</div>
             </Dialog.Panel>
           </Transition.Child>
         </div>
